26270c7706ffab351d4988c56403d6b774992dea
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-02 22:03:09 +0700

    chore: add worst case for linear searching

--- src/linear-search.ts
+++ src/linear-search.ts
@@ -1,3 +1,6 @@
+/*
+ * Worst case: The search value is in not the array
+ * */
 export default function linearSearch(haystack: number[], needle: number) {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle) return true
