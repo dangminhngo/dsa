0cc41e135f788977ed5e5650ba752aa0e01d1e2c
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-04 00:12:51 +0700

    fix: add queue

---
+++ src/queue.ts
@@ -0,0 +1,55 @@
+type Node<T> = {
+  value: T
+  next?: Node<T>
+}
+
+export default class Queue<T> {
+  constructor(
+    public length: number = 0,
+    private head?: Node<T>,
+    private tail?: Node<T>
+  ) {}
+
+  enqueue(item: T): void {
+    const node: Node<T> = { value: item }
+    this.length++
+
+    if (!this.tail) {
+      this.head = this.tail = node
+      return
+    }
+
+    this.tail.next = node
+    this.tail = node
+  }
+
+  dequeue(): T | undefined {
+    if (!this.head) return
+    this.length--
+
+    const head = this.head
+    this.head = this.head.next
+
+    if (this.length === 0) {
+      this.tail = undefined
+    }
+
+    return head.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
+
+  toString(): string {
+    let temp = this.head
+    let str = ""
+
+    while (temp) {
+      str += temp.value
+      temp = temp.next
+      str += " "
+    }
+    return str
+  }
+}
