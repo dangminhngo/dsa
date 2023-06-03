36ce19d312f9ac00f8c2f5f886f66cc4895b4bc0
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-03 09:09:23 +0700

    feat: add two crystal balls problem

---
+++ __test__/two-crystal-balls.test.ts
@@ -0,0 +1,12 @@
+import twoCrystalBalls from "~/two_crystal_balls"
+
+test("two crystal balls", () => {
+  const NUMBER_OF_FLOORS = 10_000
+  let index = Math.floor(Math.random() * NUMBER_OF_FLOORS)
+  const data = Array.from({ length: NUMBER_OF_FLOORS }, (_, x) =>
+    x < index ? false : true
+  )
+
+  expect(twoCrystalBalls(data)).toBe(index)
+  expect(twoCrystalBalls(new Array(800).fill(false))).toBe(-1)
+})
