b9117502d2c41ac12c22ca5be606af4e9979a59a
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-13 09:04:19 +0700

    fix: fix binary search

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -3,20 +3,20 @@
  * */
 export default function binarySearch(haystack: number[], needle: number) {
   let lo = 0
-  let hi = haystack.length
+  let hi = haystack.length - 1
 
-  do {
+  while (lo <= hi) {
     const mid = Math.floor((lo + hi) / 2)
     const value = haystack[mid]
 
     if (value === needle) {
       return true
     } else if (value > needle) {
-      hi = mid
+      hi = mid - 1
     } else {
       lo = mid + 1
     }
-  } while (lo < hi)
+  }
 
   return false
 }
