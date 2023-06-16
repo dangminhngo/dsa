c4fa3c770b20d64d702ff623b0b55c1979cac55d
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-16 20:46:14 +0700

    fix(dsa): fix test name

--- __tests__/dsa/insertion-sort.ts
+++ __tests__/dsa/insertion-sort.ts
@@ -1,6 +1,6 @@
 import insertionSort from "~/dsa/insertion-sort"
 
-test("buble sort", () => {
+test("insertion sort", () => {
   const arr = [1, 4, 5, 2, 9, 7, 3, 6, 8]
 
   expect(insertionSort(arr)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
