0116818c3149f17666959fbe3df2b983631f5ec5
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-16 20:50:26 +0700

    fix(dsa): remove console.log

--- dsa/selection-sort.ts
+++ dsa/selection-sort.ts
@@ -6,7 +6,6 @@ export default function selectionSort(arr: number[]) {
   for (let i = 0; i < arr.length - 1; ++i) {
     minIndex = i
     for (let j = i + 1; j < arr.length; ++j) {
-      console.log(arr)
       if (arr[j] < arr[minIndex]) {
         minIndex = j
       }
