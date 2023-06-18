a4070ebc76c2c054c59e78c9cb8f7a9f8e2458fe
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-18 14:02:08 +0700

    fix(singly linked list): use getter to get the list length

--- ds/singly-linked-list.ts
+++ ds/singly-linked-list.ts
@@ -4,16 +4,20 @@ class ListNode<T> {
 
 export default class SinglyLinkedList<T> {
   constructor(
-    public length: number = 0,
+    private size: number = 0,
     private head?: ListNode<T>,
     private tail?: ListNode<T>
   ) {}
+
+  get length(): number {
+    return this.size
+  }
 
   /*
    *  Insert a node with the value to the beginning of the list
    * */
   addFirst(value: T): void {
-    this.length++
+    this.size++
     const node = new ListNode(value)
 
     if (!this.head) {
@@ -29,7 +33,7 @@ export default class SinglyLinkedList<T> {
    * Insert a node with the value to the end of the list
    * */
   addLast(value: T): void {
-    this.length++
+    this.size++
     const node = new ListNode(value)
 
     if (!this.tail) {
@@ -45,7 +49,7 @@ export default class SinglyLinkedList<T> {
    * Return the node value at the specified index. If not, return undefined
    * */
   get(index: number): T | undefined {
-    if (index < 0 || index >= this.length) return undefined
+    if (index < 0 || index >= this.size) return undefined
 
     for (let curr = this.head, count = 0; curr; curr = curr.next, count++) {
       if (count === index) {
@@ -95,12 +99,12 @@ export default class SinglyLinkedList<T> {
    * Remove a node at the specified index, return boolean
    * */
   removeAt(index: number): T | undefined {
-    if (index < 0 || index >= this.length) {
+    if (index < 0 || index >= this.size) {
       return
     }
 
     if (index === 0) {
-      this.length = Math.max(0, this.length - 1)
+      this.size = Math.max(0, this.size - 1)
       const head = this.head
       this.head = this.head?.next
       return head?.value
@@ -114,8 +118,8 @@ export default class SinglyLinkedList<T> {
       prev = curr, curr = curr.next, count++
     ) {
       if (count === index && prev) {
-        this.length = Math.max(0, this.length - 1)
-        if (index === this.length - 1) {
+        this.size = Math.max(0, this.size - 1)
+        if (index === this.size - 1) {
           this.tail = prev
         }
         prev.next = curr.next
@@ -131,7 +135,7 @@ export default class SinglyLinkedList<T> {
    * */
   remove(value: T): boolean {
     if (this.head?.value === 0) {
-      this.length = Math.max(0, this.length - 1)
+      this.size = Math.max(0, this.size - 1)
       this.head = this.head?.next
       return true
     }
@@ -143,7 +147,7 @@ export default class SinglyLinkedList<T> {
       prev = curr, curr = curr.next
     ) {
       if (curr.value === value && prev) {
-        this.length = Math.max(0, this.length - 1)
+        this.size = Math.max(0, this.size - 1)
 
         if (this.tail?.value === value) {
           this.tail = prev
@@ -173,7 +177,7 @@ export default class SinglyLinkedList<T> {
    * Set an element at the specified index
    * */
   set(index: number, value: T): T | undefined {
-    if (index < 0 || index >= this.length) return
+    if (index < 0 || index >= this.size) return
 
     for (let curr = this.head, count = 0; curr; curr = curr.next, count++) {
       if (count === index) {
